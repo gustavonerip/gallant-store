d05099064027816261e1044c8bd9eca8757873c7
Gustavo <gustavoneripeixoto@gmail.com>
2023-04-14 15:20:34 -0700

    Add to basket functionallity working

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 // GENERATE SHOP ITEMS
 let shop = document.getElementById("product-gallery");
+let cartTotalAmountInfo = document.getElementById("cart-total-info");
+let shoppingCartList = document.getElementById("cart-items");
 let cartList = JSON.parse(localStorage.getItem("shopping-cart")) || [];
 
 
@@ -26,24 +28,64 @@ let generateShop = () => {
   };
   
 generateShop();
+
+let generateCartItems = () => {
+    if (cartList.length !== 0) {
+      return (shoppingCartList.innerHTML = cartList
+        .map((x) => {
+          let { id, item } = x;
+          let search = shopItemsData.find((y) => y.id === id) || [];
+          return `
+          <div id="product-id-${id}" class="cart-item">
+          <div class="cart-item-thumbnail">
+              <img src=${search.img} alt="">
+          </div>
+          <div class="cart-item-info">
+              <h3 class="cart-item-title">${search.title} <span class="cart-item-price"><i class="fa-solid fa-tag"></i> $${search.price}</span></h3>
+              <div class="cart-item-quantity">
+                  <i onclick="decrement(${id})" class="fa-solid fa-minus"></i>
+                  <p id="item-id-${id}">${item}</p>
+                  <i onclick="increment(${id})" class="fa-solid fa-plus"></i>
+              </div>
+              <p class="cart-item-total-price">Subtotal: <span>$${item * search.price}</span></p>
+          </div>
+
+          <div onclick="removeItem(${id})" class="cart-item-delete"><i class="fa-solid fa-trash-can"></i></div>
+
+      </div>
+        `;
+        })
+        .join(""));
+    } else {
+      shoppingCartList.innerHTML = `
+      <div class="empty-cart">
+        <p>Your Cart is Empty</p>
+        <button class="checkout-btn" onclick="closeCart()">Start Shopping</button>
+      </div>
+      `;
+      cartTotalAmountInfo.innerHTML = ``;
+    }
+  };
+  
+generateCartItems();
   
 // FUNCTION TO INCREASE ITEM QUANTITY IN THE CART
 
 let increment = (id) => {
     let selectedItem = id;
     let search = cartList.find((x) => x.id === selectedItem.id);
-  
+
     if (search === undefined) {
         cartList.push({
         id: selectedItem.id,
         item: 1,
-      });
+        });
     } else {
-      search.item += 1;
+        search.item += 1;
     }
-  
-    // UPDATE LOCAL STORAGE
+    generateCartItems();
     update(selectedItem.id);
+    updateCartItem(selectedItem.id);
     localStorage.setItem("shopping-cart", JSON.stringify(cartList));
 };
 
@@ -60,6 +102,7 @@ let decrement = (id) => {
 
     // UPDATE LOCAL STORAGE
     update(selectedItem.id);
+    updateCartItem(selectedItem.id);
     cartList = cartList.filter((x) => x.item !== 0);
     localStorage.setItem("shopping-cart", JSON.stringify(cartList));
 };
@@ -67,10 +110,20 @@ let decrement = (id) => {
 // UPDATE CART ITEMS QUANTITY
 let update = (id) => {
     let search = cartList.find((x) => x.id === id);
-    // console.log(search.item);
     document.getElementById(id).innerHTML = search.item;
     calculation();
+    TotalAmount();
 };
+
+// UPDATE ITEMS LIST
+
+let updateCartItem = (id) => {
+    let search = cartList.find((x) => x.id === id);
+    document.getElementById("item-id-"+id).innerHTML = search.item;
+    updateCartItem(document.getElementById("item-id-"+id));
+    calculation();
+    TotalAmount();
+  };
 
 // SIMPLE CALCULATION FUNCTION
 let calculation = () => {
@@ -78,6 +131,45 @@ let calculation = () => {
     cartIcon.innerHTML = cartList.map((x) => x.item).reduce((x, y) => x + y, 0);
 };
 calculation();
+
+let removeItem = (id) => {
+    let selectedItem = id;
+    cartList = cartList.filter((x) => x.id !== selectedItem.id);
+    generateCartItems();
+    generateShop();
+    TotalAmount();
+    calculation();
+    localStorage.setItem("shopping-cart", JSON.stringify(cartList));
+};
+  
+let clearCart = () => {
+    cartList = [];
+    generateCartItems();
+    generateShop();
+    calculation();
+    closeCart();
+    localStorage.setItem("shopping-cart", JSON.stringify(cartList));
+};
+
+let TotalAmount = () => {
+    if (cartList.length !== 0) {
+      let amount = cartList
+        .map((x) => {
+          let { item, id } = x;
+          let search = shopItemsData.find((y) => y.id === id) || [];
+  
+          return item * search.price;
+        })
+        .reduce((x, y) => x + y, 0);
+      // console.log(amount);
+      cartTotalAmountInfo.innerHTML = `
+        <p>Total: <span>$${amount}</span></p>
+        <button class="checkout-btn"><i class="fa-solid fa-credit-card"></i> Checkout</button>
+      `;
+    } else return;
+  };
+  
+  TotalAmount();
 
 // OPEN & CLOSE CART WINDOW
 const cartBtn = document.querySelector(".cart-btn");
@@ -99,3 +191,8 @@ overlay.addEventListener('click', ()=>{
     cartWindow.classList.toggle('cart-open');
     overlay.classList.toggle('cart-open');
 });
+
+let closeCart = ()=>{
+    cartWindow.classList.toggle('cart-open');
+    overlay.classList.toggle('cart-open');
+}
