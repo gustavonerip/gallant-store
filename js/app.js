a5c5a0016fa5aff2386f45ffb9497af6bb5e809d
Gustavo <gustavoneripeixoto@gmail.com>
2023-04-14 12:39:32 -0700

    Product gallery and add to the cart completed

--- js/app.js
+++ js/app.js
@@ -1,15 +1,101 @@
+// GENERATE SHOP ITEMS
+let shop = document.getElementById("product-gallery");
+let cartList = JSON.parse(localStorage.getItem("shopping-cart")) || [];
 
-// OPEN - CLOSE CART WINDOW
+
+let generateShop = () => {
+    return (shop.innerHTML = shopItemsData
+      .map((x) => {
+        let { id, title, price, desc, img } = x;
+        let search = cartList.find((x) => x.id === id) || [];
+        return `
+        <div id="product-id-${id}" class="product-card">
+            <div class="product-image"><img src=${img} alt=${title}></div>
+            <h3 class="product-title">${title}</h3>
+            <p class="product-price"><i class="fa-solid fa-tag"></i> ${price}</p>
+            <div class="product-item-quantity">
+                <i onclick="decrement(${id})" class="fa-solid fa-minus"></i>
+                <p id="${id}" >${search.item === undefined ? 0 : search.item}</p>
+                <i onclick="increment(${id})" class="fa-solid fa-plus"></i>
+            </div>
+            <button onclick="increment(${id})" class="btn" id="add-to-cart-btn-${id}"><i class="fa-solid fa-plus"></i> Add to Cart</button>
+        </div>
+        `;
+      })
+      .join(""));
+  };
+  
+generateShop();
+  
+// FUNCTION TO INCREASE ITEM QUANTITY IN THE CART
+
+let increment = (id) => {
+    let selectedItem = id;
+    let search = cartList.find((x) => x.id === selectedItem.id);
+  
+    if (search === undefined) {
+        cartList.push({
+        id: selectedItem.id,
+        item: 1,
+      });
+    } else {
+      search.item += 1;
+    }
+  
+    // UPDATE LOCAL STORAGE
+    update(selectedItem.id);
+    localStorage.setItem("shopping-cart", JSON.stringify(cartList));
+};
+
+// FUNCTION TO DECREASE ITEM QUANTITY IN THE CART
+let decrement = (id) => {
+    let selectedItem = id;
+    let search = cartList.find((x) => x.id === selectedItem.id);
+  
+    if (search === undefined) return;
+    else if (search.item === 0) return;
+    else {
+      search.item -= 1;
+    }
+
+    // UPDATE LOCAL STORAGE
+    update(selectedItem.id);
+    cartList = cartList.filter((x) => x.item !== 0);
+    localStorage.setItem("shopping-cart", JSON.stringify(cartList));
+};
+
+// UPDATE CART ITEMS QUANTITY
+let update = (id) => {
+    let search = cartList.find((x) => x.id === id);
+    // console.log(search.item);
+    document.getElementById(id).innerHTML = search.item;
+    calculation();
+};
+
+// SIMPLE CALCULATION FUNCTION
+let calculation = () => {
+    let cartIcon = document.getElementById("cartAmount");
+    cartIcon.innerHTML = cartList.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
+calculation();
+
+// OPEN & CLOSE CART WINDOW
 const cartBtn = document.querySelector(".cart-btn");
 const cartWindow = document.querySelector(".cart-window");
 const closeCartBtn = document.querySelector(".close-cart-btn");
+const overlay = document.querySelector(".overlay");
 
 cartBtn.addEventListener('click', ()=>{
     cartWindow.classList.toggle('cart-open');
-    document.querySelector(".overlay").classList.toggle('cart-open');
+    overlay.classList.toggle('cart-open');
 });
 
 closeCartBtn.addEventListener('click', ()=>{
     cartWindow.classList.toggle('cart-open');
-    document.querySelector(".overlay").classList.toggle('cart-open');
+    overlay.classList.toggle('cart-open');
+});
+
+overlay.addEventListener('click', ()=>{
+    cartWindow.classList.toggle('cart-open');
+    overlay.classList.toggle('cart-open');
 });
---
+++ js/products.js
@@ -0,0 +1,66 @@
+let shopItemsData = [
+{
+    id: "gllt140423956",
+    category: "Tops",
+    title: "Dress Shirts",
+    price: "120",
+    desc: "Best dress shirts in the city!",
+    img: "./images/products/dress-shirt.jpeg"
+},
+{
+    id: "gllt140423958",
+    category: "Pants",
+    title: "Pants",
+    price: "100",
+    desc: "Best pants in the city!",
+    img: "./images/products/chinos-2.jpeg"
+},
+{
+    id: "gllt140423959",
+    category: "Tops",
+    title: "Casual Shirts",
+    price: "100",
+    desc: "Best casual shirts in the city!",
+    img: "./images/products/casual-shirt.jpeg"
+},
+{
+    id: "gllt140423960",
+    category: "Tops",
+    title: "Jackets",
+    price: "150",
+    desc: "Best jackets in the city!",
+    img: "./images/products/jackets.jpeg"
+},
+{
+    id: "gllt140423961",
+    category: "Tops",
+    title: "Casual Shirts",
+    price: "50",
+    desc: "Best polo shirts in the city!",
+    img: "./images/products/polo.webp"
+},
+{
+    id: "gllt140423962",
+    category: "Suits",
+    title: "Suits",
+    price: "500",
+    desc: "Best suits in the city!",
+    img: "./images/products/suits.jpg"
+},
+{
+    id: "gllt140423963",
+    category: "Tops",
+    title: "Print Shirts",
+    price: "80",
+    desc: "Best print shirts in the city!",
+    img: "./images/products/print-shirt.webp"
+},
+{
+    id: "gllt140423964",
+    category: "Shoes",
+    title: "Shoes",
+    price: "200",
+    desc: "Best shoes in the city!",
+    img: "./images/products/shoes.webp"
+},
+]
